f3a6e1174d33238faa5301fbb050423393904e0a
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-11-30 18:35:50 +0000

    Initial Commit

---
+++ Damping/DampingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DampingApp.swift
+//  Damping
+//
+//  Created by Jack Finnis on 30/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DampingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
